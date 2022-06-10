667a05933c4822cb1ba8a2e33234b6ae0a5a9d05
twinoxen <1854037+twinoxen@users.noreply.github.com>
2022-06-10 12:23:45 -0400

    experiment circle drawing

--- sketch-02.js
+++ sketch-02.js
@@ -1,8 +1,5 @@
 const canvasSketch = require('canvas-sketch');
-const {
-  random,
-  math: { degToRad },
-} = require('canvas-sketch-util');
+const { random, math } = require('canvas-sketch-util');
 
 const settings = {
   dimensions: [1080, 1080],
@@ -10,18 +7,18 @@ const settings = {
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    const cx = width * 0.5;
-    const cy = height * 0.5;
+    const cx = width * 0.1;
+    const cy = height * 0.1;
     const w = width * 0.01;
     const h = width * 0.1;
 
     let x, y;
 
-    const sliceCount = 12;
+    const sliceCount = 24;
     const radius = width * 0.3;
 
     new Array(sliceCount).fill().forEach((item, index) => {
-      const slice = degToRad(360 / sliceCount);
+      const slice = math.degToRad(360 / sliceCount);
       const angle = slice * index;
 
       x = cx + radius * Math.sin(angle);
@@ -31,12 +28,30 @@ const sketch = () => {
 
       context.translate(x, y);
       context.rotate(-angle);
-      context.scale(random.range(1, 3), random.range(1, 3));
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
 
       context.beginPath();
-      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
       context.fillStyle = 'black';
       context.fill();
+
+      context.restore();
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5, 20);
+
+      context.beginPath();
+      context.arc(
+        0,
+        0,
+        random.range(radius * 0.7, radius * 1.3),
+        slice * random.range(1, -8),
+        slice * random.range(1, 5)
+      );
+      context.stroke();
 
       context.restore();
     });
