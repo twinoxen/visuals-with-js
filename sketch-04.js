9661a3dd2981a1e24c6115a24207f65c7179c67f
Jonathan Werner <jonathanwerner@JonathasonalMBP.attlocal.net>
2022-06-11 21:37:13 -0400

    finish tweak pane

--- sketch-04.js
+++ sketch-04.js
@@ -1,9 +1,23 @@
 const canvasSketch = require('canvas-sketch');
 const { random, math } = require('canvas-sketch-util');
+const { Pane } = require('tweakpane');
 
 const settings = {
   dimensions: [1080, 1080],
   animate: true,
+};
+
+const PARAMS = {
+  speed: 0.5,
+  columns: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  freq: 0.001,
+  amp: 2,
+  frame: 0,
+  animate: true,
+  lineCap: 'butt',
 };
 
 const sketch = () => {
@@ -13,8 +27,8 @@ const sketch = () => {
 
     const DrawGrid = GridDrawer(context);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = PARAMS.columns;
+    const rows = PARAMS.rows;
     const numCells = cols * rows;
 
     const gridWidth = width * 0.8;
@@ -38,8 +52,12 @@ const sketch = () => {
         const w = cellWidth * 0.8;
         const h = cellHeight * 0.8;
 
-        const n = random.noise2D(x + frame * 10, y, 0.001);
-        const s = math.mapRange(n, -1, 1, 1, 30);
+        const f = PARAMS.animate ? frame : PARAMS.frame;
+
+        // const n = random.noise2D(x + frame * 10, y, 0.001);
+        const n = random.noise3D(x, y, f * 10, PARAMS.freq, PARAMS.amp);
+
+        const s = math.mapRange(n, -1, 1, PARAMS.scaleMin, PARAMS.scaleMax);
         const r = n * Math.PI * 0.2;
 
         DrawGrid(x, y, w, h, r, s);
@@ -58,6 +76,7 @@ function GridDrawer(context) {
     context.translate(x, y);
     context.rotate(r);
     context.lineWidth = s;
+    context.lineCap = PARAMS.lineCap;
     context.beginPath();
     context.moveTo(w * -0.5, 0);
     context.lineTo(w * 0.5, 0);
@@ -66,4 +85,30 @@ function GridDrawer(context) {
   };
 }
 
+function SetupPane() {
+  const pane = new Pane({
+    title: 'Parameters',
+  });
+
+  const f1 = pane.addFolder({
+    title: 'Grid',
+  });
+
+  f1.addInput(PARAMS, 'columns', { min: 1, max: 200, step: 1 });
+  f1.addInput(PARAMS, 'rows', { min: 1, max: 200, step: 1 });
+
+  const f2 = pane.addFolder({
+    title: 'Transforms',
+  });
+
+  f2.addInput(PARAMS, 'scaleMin', { min: 1, max: 999 });
+  f2.addInput(PARAMS, 'scaleMax', { min: 1, max: 999 });
+  f2.addInput(PARAMS, 'freq', { min: 0.001, max: 0.01 });
+  f2.addInput(PARAMS, 'animate')
+  f2.addInput(PARAMS, 'frame', { min: 1, max: 999, step: 1});
+  f2.addInput(PARAMS, 'amp', { min: 0.001, max: 5});
+  f2.addInput(PARAMS, 'lineCap', { options: { butt: 'butt', round: 'round', square: 'square' }});
+}
+
+SetupPane();
 canvasSketch(sketch, settings);
